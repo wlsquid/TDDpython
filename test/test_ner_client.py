7fb666647225d88082ea173609095a5cc01a86cd
wlsquid <wyndhammorland@gmail.com>
2022-02-05 17:12:39 +1000

    TEST PASS WOO

--- test/test_ner_client.py
+++ test/test_ner_client.py
@@ -1,7 +1,9 @@
 import unittest
-
+from ner_client import NamedEntityClient
 class TestNerClient(unittest.TestCase):
-
+    # { ents: [{...}],
+    #   html: "<span>"  }
+    #
     def test_get_ents_returns_dicitonary_given_empty_string(self):
         ner = NamedEntityClient()
         ents = ner.get_ents("")
